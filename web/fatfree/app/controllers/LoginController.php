d4ccf5a1aad9a13f3b2fc723b97c858a5a77f72f
Drew Budwin <drew@budw.in>
2016-10-10 18:33:55 -0400

    Fix putting brackets on their own line for classes and methods

--- web/fatfree/app/controllers/LoginController.php
+++ web/fatfree/app/controllers/LoginController.php
@@ -2,12 +2,16 @@
 
 namespace Controllers;
 
-class LoginController extends Controller {
-    function beforeRoute() {
+class LoginController extends Controller
+{
+    function beforeRoute()
+    {
         //Overridden to prevent endless redirects if a user is redirected to the login page for not being logged in
 
-        if ($this->f3->get("SESSION.user") !== null ) {
-            if ($this->f3->get("ALIAS") == "loginPage") {
+        if ($this->f3->get("SESSION.user") !== null )
+        {
+            if ($this->f3->get("ALIAS") == "loginPage")
+            {
                 $this->f3->reroute("@devices");
             }
         }
@@ -19,7 +23,8 @@ class LoginController extends Controller {
         echo $template->render("index.html");
     }
 
-    function login($f3) {
+    function login($f3)
+    {
         parse_str($_SERVER["QUERY_STRING"]);
 
         //Verify that the access token belongs to us
@@ -29,7 +34,7 @@ class LoginController extends Controller {
         $r = curl_exec($c);
         curl_close($c);
         $d = json_decode($r);
-            
+
         if ($d->aud != $f3->get("AMAZON_TOKEN")) {
             //The access token does not belong to us
             header('HTTP/1.1 404 Not Found');
@@ -52,7 +57,7 @@ class LoginController extends Controller {
 
         $currentUser = $userModel->findUser($d->user_id)[0];
 
-        if ($currentUser === NULL) {
+        if ($currentUser === null) {
             $userModel->createNewUser($d->name, $d->email, $d->user_id);
             $currentUser = $userModel->findUser($d->user_id)[0];
         }
@@ -62,7 +67,8 @@ class LoginController extends Controller {
         $f3->reroute("@devices");
     }
 
-    function logout($f3) {
+    function logout($f3)
+    {
         $f3->clear("SESSION.user");
         $f3->reroute("@loginPage");
     }
